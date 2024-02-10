057855e39e3f43eb384195dd1b7d93debd2760f0
Patrick Swickard <swickape@hotmail.com>
2024-02-10 12:37:10 -0500

    checkin le code

--- generate_word_cloud_from_le.py
+++ generate_word_cloud_from_le.py
@@ -15,7 +15,7 @@ URL = 'https://www.gutenberg.org/cache/epub/541/pg541.txt'
 
 def plot_wordcloud(wordcloud):
   # plot the WordCloud image
-  plt.figure(figsize = (8, 8), facecolor = None)
+  plt.figure(figsize = (18, 24), dpi=300,facecolor = None)
   plt.imshow(wordcloud)
   plt.axis("off")
   plt.tight_layout(pad = 0)
@@ -25,25 +25,26 @@ def plot_wordcloud(wordcloud):
 
 def save_wordcloud(wordcloud,string):
   # plot the WordCloud image
-  plt.figure(figsize = (8, 8), facecolor = None)
+  plt.figure(figsize = (18, 24), facecolor = None)
   plt.imshow(wordcloud)
   plt.axis("off")
   plt.tight_layout(pad = 0)
-  plt.savefig('cache/' + str(string) + '.png')
+  plt.savefig('cache/le/' + str(string) + '.png')
 #  # this is necessary without jupyter
 #  plt.show()
   plt.close()
 
-def get_all_litlist():
-  df = pd.read_csv('litlist.csv')
+def get_all_litlistle():
+  df = pd.read_csv('litlistle.csv')
   print(df)
 
   for index,row in df.iterrows():
-    if index >= 156:
+    if index >= 180:
       title = row['title']
+      desc = row['desc']
       author = row['author']
       url = row['url']
-      text = wordcloud_utils.get_text_from_pg(url)
+      text = get_text_from_le(url)
       wordcloud = wordcloud_utils.generate_word_cloud_from_text(text)
       save_wordcloud(wordcloud,index)
 
@@ -57,7 +58,7 @@ def get_text_from_le(url):
   print(numpages)
   rawtextlist = []
   for i in range(1,int(numpages) + 1):
-    thisurl = URL + '/?page=' + str(i)
+    thisurl = url + '/?page=' + str(i)
     print('trying ' + thisurl)
     rawtext = wordcloud_utils.get_text_from_url(thisurl)
     rawtextlist.append(rawtext)
@@ -71,7 +72,7 @@ def get_text_from_le(url):
   return foundclean
 
 def get_numpages_from_le(url):
-  rawtext = wordcloud_utils.get_text_from_url(URL)
+  rawtext = wordcloud_utils.get_text_from_url(url)
   resultre = re.findall(r"<a\s+class=\"l_bJ\"[^>]*>\s*(\d+)\s*</a>\s*<form",rawtext,re.DOTALL)
   if resultre:
     result = resultre[0]
@@ -85,12 +86,12 @@ URL = 'https://www.literotica.com/s/the-birth-of-bella-the-bimbo-clown'
 
 ###print(get_numpages_from_le(URL))
 
-wordcloud = get_wc_from_le(URL)
+#wordcloud = get_wc_from_le(URL)
 
 #wordcloud = wordcloud_utils.generate_word_cloud_from_french_text(text)
 
-plot_wordcloud(wordcloud)
+#plot_wordcloud(wordcloud)
 
 #save_wordcloud(wordcloud,1)
 
-#get_all_litlist()
+get_all_litlistle()
